3d53e632ce17e96fdcab14e6d6c2ec607e727533
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2025-07-12 22:47:32 +0530

    Time: 0 ms (100%), Space: 47 MB (9.23%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,5 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
+        int i = 0; // Pointer for placement
+
+                        for (int num : nums) {
+                                    // If i < 2, we can always add the number
+                                                // If num != nums[i-2], it means this is not the 3rd+ duplicate
+                                                            if (i < 2 || num > nums[i - 2]) {
+                                                                            nums[i] = num;
+                                                                                            i++;
+                                                                                                        }
+                                                                                                                }
+
+                                                                                                                        return i; // New length of the array
     }
 }
