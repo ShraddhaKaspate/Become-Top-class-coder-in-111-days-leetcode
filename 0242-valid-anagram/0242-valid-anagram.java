ff8bc3f1e5e252191634ba28ab85197f77039775
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2025-07-13 23:50:29 +0530

    Time: 19 ms (12.9%), Space: 44.6 MB (43.11%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+     
+        for (char c : s.toCharArray()) {
+        
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1); 
+            }
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false; 
+            }
+            map.put(c, map.get(c) - 1);
+            if (map.get(c) < 0) {
+                return false; 
+            }
+        }
+
+ 
+        for (int count : map.values()) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true; 
+    }
+}
