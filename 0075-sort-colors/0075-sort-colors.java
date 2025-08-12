4c6eabd9a5ee755493039fa4a87295866ca73f65
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2025-08-12 22:49:42 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.01%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void sortColors(int[] a) {
+        int l = 0, m = 0, h = a.length - 1;
+        while (m <= h) {
+            if (a[m] == 0) {
+                int t = a[l]; a[l++] = a[m]; a[m++] = t;
+            } else if (a[m] == 2) {
+                int t = a[m]; a[m] = a[h]; a[h--] = t;
+            } else m++;
+        }
+    }
+}
