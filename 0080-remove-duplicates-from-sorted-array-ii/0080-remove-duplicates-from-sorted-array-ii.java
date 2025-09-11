bf1531f01eab84acbdca5a3b57c6df118b595720
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2025-09-11 09:01:37 +0530

    Update 0080-remove-duplicates-from-sorted-array-ii.java

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,16 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i = 0; // Pointer for placement
+        int i = 0; 
 
-                        for (int num : nums) {
-                                    // If i < 2, we can always add the number
-                                                // If num != nums[i-2], it means this is not the 3rd+ duplicate
-                                                            if (i < 2 || num > nums[i - 2]) {
-                                                                            nums[i] = num;
-                                                                                            i++;
-                                                                                                        }
-                                                                                                                }
+          for (int num : nums) {
+            if (i < 2 || num > nums[i - 2]) {
+                     nums[i] = num;
+                     i++;
+            }
+        }
 
-                                                                                                                        return i; // New length of the array
+    return i; 
     }
 }
