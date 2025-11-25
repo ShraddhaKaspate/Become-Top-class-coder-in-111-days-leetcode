a20920f6d9c20c9f8057c37396833c3e9396dc1d
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2025-11-26 00:20:14 +0530

    Time: 4 ms (99.71%), Space: 47.6 MB (31.75%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean validPalindrome(String s) {
         int i=0,j=s.length()-1;
+        /*
         while(i<j && s.charAt(i)==s.charAt(j)){
             
                 i++;
@@ -27,5 +28,32 @@ class Solution {
                  
         
         return false;
+        */
+
+        while(i<=j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }else{
+
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+            }
+        }
+        return true;
     }
+
+    public boolean isPalindrome(String s,int i, int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+
+            }else{
+           i++;
+           j--;
+           
+        }
+    }
+    return true;
 }
+}
+
