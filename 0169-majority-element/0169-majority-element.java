8437fc64de1b1ad2938954661664aa7ddfd3549c
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2026-04-02 00:02:54 +0530

    Time: 13 ms (29.11%), Space: 52.9 MB (76.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) 
     {
+        /*
         int count = 0;
         int candidate = 0;
 
@@ -17,5 +18,19 @@ class Solution {
         }
 
         return candidate;
+        */
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+
+        for(int key: map.keySet()){
+            if(map.get(key)>nums.length/2){
+                return key;
+            }
+        }
+        return -1;
     }
 }
